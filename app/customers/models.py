b3f4d3385cabf058aa5df04aacee11d84e2ffa23
Dario Olazar <lernny@gmail.com>
2021-02-23 01:13:25 -0300

    Create index views
    Add templates folder to settings
    Update model, removing unique import
    Create url for home

--- app/customers/models.py
+++ app/customers/models.py
@@ -1,14 +1,16 @@
-from enum import unique
 from django.db import models
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 class Customer(models.Model):
+    """ Model representing a customer  """
     class Meta:
         verbose_name = _('cutomer')
+        ordering = ['name']
 
     name = models.CharField(_('name'), max_length=40)
-    ruc = models.CharField(_('ruc'), max_length=40, unique=True)
-    email = models.EmailField(_('email', unique=True))
+    ruc = models.CharField(_('ruc'), max_length=40, unique=True, null=True, blank=True)
+    email = models.EmailField(_('email'), unique=True)
     telephone = models.CharField(_('telephone'), max_length=40, blank=True, null=True)
     address = models.CharField(_('address'), max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
@@ -16,3 +18,6 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('customer-detail', args=[str(self.id)])
