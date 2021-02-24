91284f8bac3e3952d631e61082e1379fe286430f
Dario Olazar <lernny@gmail.com>
2021-02-24 10:03:43 -0300

    Add customer model to django-admin

--- app/customers/admin.py
+++ app/customers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from customers.models import Customer
+
+@admin.register(Customer)
+class CustomersAdmin(admin.ModelAdmin):
+    pass
